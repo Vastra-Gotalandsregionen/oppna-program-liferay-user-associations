e110c36d0d1f6b5b0d865cd2a9cb2dbf2ef05264
wmdaros <wmdaros@32dd8a1c-4626-fe3b-c9c0-0d2da1f31c85>
2011-06-15 14:46:42 +0000

    - Login redirect based on group (Community)

--- trunk/core-bc/modules/portlet/src/main/java/se/vgregion/userassociations/hook/UserLoginAction.java
+++ trunk/core-bc/modules/portlet/src/main/java/se/vgregion/userassociations/hook/UserLoginAction.java
@@ -2,14 +2,19 @@ package se.vgregion.userassociations.hook;
 
 import com.liferay.portal.kernel.events.Action;
 import com.liferay.portal.kernel.events.ActionException;
+import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.struts.LastPath;
 import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.PropsUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.model.Group;
+import com.liferay.portal.model.Role;
 import com.liferay.portal.model.User;
 import com.liferay.portal.service.UserLocalServiceUtil;
 import com.liferay.portal.util.PortalUtil;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import se.vgregion.userassociations.matcher.Matcher;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -23,6 +28,15 @@ import java.util.List;
  * Time: 15:47
  */
 public class UserLoginAction extends Action {
+
+    private List<Matcher> matcherList;
+
+    public UserLoginAction() {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        matcherList = (List<Matcher>) ctx.getBean("matcherList");
+    }
+
     @Override
     public void run(HttpServletRequest request, HttpServletResponse response) throws ActionException {
         try {
@@ -51,19 +65,20 @@ public class UserLoginAction extends Action {
 //                        break;
 //                }
 //            }
-
             LastPath lastPath = (LastPath) session.getAttribute(WebKeys.LAST_PATH);
 
-            if (lastPath == null) {
-                // Look for Communities
-                List<Group> groups = user.getGroups();
-                for (Group group : groups) {
-                    if (firstUserGroup == null)
-                        firstUserGroup = group;
-                    if (group.hasPrivateLayouts())
-                        lastPath = new LastPath(request.getContextPath(), PropsUtil.get(PropsKeys.LAYOUT_FRIENDLY_URL_PRIVATE_GROUP_SERVLET_MAPPING) + group.getFriendlyURL());
-                    if (lastPath != null)
-                        break;
+            if (noLanding(user)) {
+                lastPath = new LastPath(request.getContextPath(), "web/guest/home");
+            } else {
+                if (lastPath.getPath() == null || lastPath.getPath().equals("/")) {
+                    // Look for Communities
+                    List<Group> groups = user.getGroups();
+                    for (Group group : groups) {
+                        if (group.hasPrivateLayouts())
+                            lastPath = new LastPath(request.getContextPath(), PropsUtil.get(PropsKeys.LAYOUT_FRIENDLY_URL_PRIVATE_GROUP_SERVLET_MAPPING) + group.getFriendlyURL());
+                        if (lastPath != null)
+                            break;
+                    }
                 }
             }
 
@@ -75,11 +90,21 @@ public class UserLoginAction extends Action {
         }
     }
 
+    private boolean noLanding(User user) {
+        try {
+            for (Role role : user.getRoles()) {
+                if (role.getName().equals("Administrator")) return true;
+            }
+        } catch (SystemException e) {
+            e.printStackTrace();
+        }
+
+        return user.isDefaultUser() || !user.isActive() || user.isLockout();
+    }
+
     private void resolveAssociations(User user) {
-        //repository fetches all matchers
-
-        //for each matcher, run each matcher
-        //each matcher adds appropriate groups to the user
-
+        for (Matcher matcher : matcherList) {
+            matcher.process(user);
+        }
     }
 }
--- trunk/core-bc/modules/portlet/src/main/java/se/vgregion/userassociations/matcher/UserScreenNameGroupMatcher.java
+++ trunk/core-bc/modules/portlet/src/main/java/se/vgregion/userassociations/matcher/UserScreenNameGroupMatcher.java
@@ -3,37 +3,97 @@ package se.vgregion.userassociations.matcher;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.model.User;
+import com.liferay.portal.service.GroupLocalService;
+import com.liferay.portal.service.GroupLocalServiceUtil;
 import com.liferay.portal.service.UserLocalService;
+import com.liferay.portal.service.UserLocalServiceUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 
-/**
- * User: pabe
- * Date: 2011-06-15
- * Time: 09:21
- */
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
 public class UserScreenNameGroupMatcher implements Matcher {
 
+    private long companyId;
     private String valueRegExp;
-    private long[] groupIds;
-
+    private List<Long> groupIds = new ArrayList<Long>();
+    private List<String> groupNames;
 
     private UserLocalService userLocalService;
+    private GroupLocalService groupLocalService;
 
-    public UserScreenNameGroupMatcher(String valueRegExp, long[] groupIds) {
+    public UserScreenNameGroupMatcher(long companyId, String valueRegExp, List<String> groupNames) {
+        this.companyId = companyId;
         this.valueRegExp = valueRegExp;
-        this.groupIds = groupIds;
+        this.groupNames = groupNames;
     }
 
     public void process(User user) {
+        if (companyId != user.getCompanyId()) return;
+
         if (user.getScreenName().matches(valueRegExp)) {
-            for (long groupId : groupIds) {
-                try {
-                    userLocalService.addGroupUsers(groupId, new long[]{user.getUserId()});
-                } catch (PortalException e) {
-                    e.printStackTrace();
-                } catch (SystemException e) {
-                    e.printStackTrace();
+            long[] memberIn = null;
+            try {
+                memberIn = user.getGroupIds();
+
+                for (long groupId : groupIds) {
+                    if (alreadyMember(groupId, memberIn)) continue;
+
+                    try {
+                        getUserLocalService().addGroupUsers(groupId, new long[]{user.getUserId()});
+                    } catch (PortalException e) {
+                        e.printStackTrace();
+                    } catch (SystemException e) {
+                        e.printStackTrace();
+                    }
                 }
+            } catch (PortalException e) {
+                e.printStackTrace();
+            } catch (SystemException e) {
+                e.printStackTrace();
             }
         }
+    }
+
+    private void initGroupIds() {
+        for (String groupName : groupNames) {
+            try {
+                groupIds.add(getGroupLocalService().getGroup(companyId, groupName).getGroupId());
+            } catch (Exception e) {
+                // Group not available in this context
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private boolean alreadyMember(long groupId, long[] memberIn) {
+        for (long member: memberIn) {
+            if (member == groupId) return true;
+        }
+        return false;
+    }
+
+    /*
+     * This cannot be autowired because then UserLocalServiceUtil would be called before the UserLocalService
+     * instance has been created.
+     *
+     * @return
+     */
+    private UserLocalService getUserLocalService() {
+        if (userLocalService == null) {
+            userLocalService = UserLocalServiceUtil.getService();
+        }
+        return userLocalService;
+    }
+
+    private GroupLocalService getGroupLocalService() {
+        if (groupLocalService == null) {
+            groupLocalService = GroupLocalServiceUtil.getService();
+        }
+        return groupLocalService;
     }
 }
