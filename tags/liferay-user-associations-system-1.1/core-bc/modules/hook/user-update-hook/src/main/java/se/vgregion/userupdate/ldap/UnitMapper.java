6564d2e55d5db4e863c070ac6b246e0a2ea1c5ba
simon.goransson@gmail.com <simon.goransson@gmail.com@32dd8a1c-4626-fe3b-c9c0-0d2da1f31c85>
2011-12-07 13:40:45 +0000

    [maven-release-plugin]  copy for tag liferay-user-associations-system-1.1

---
+++ tags/liferay-user-associations-system-1.1/core-bc/modules/hook/user-update-hook/src/main/java/se/vgregion/userupdate/ldap/UnitMapper.java
@@ -0,0 +1,24 @@
+package se.vgregion.userupdate.ldap;
+
+import org.springframework.ldap.core.DirContextAdapter;
+import org.springframework.ldap.core.simple.ParameterizedContextMapper;
+import se.vgregion.userupdate.domain.UnitLdapAttributes;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Created: 2011-11-29 14:28
+ *
+ * @author <a href="mailto:david.rosell@redpill-linpro.com">David Rosell</a>
+ */
+public class UnitMapper implements ParameterizedContextMapper<UnitLdapAttributes> {
+    @Override
+    public UnitLdapAttributes mapFromContext(Object ctx) {
+        DirContextAdapter adapter = (DirContextAdapter) ctx;
+
+        UnitLdapAttributes unit = new UnitLdapAttributes();
+        unit.setDn(adapter.getDn());
+        unit.setVgrVardVal(adapter.getStringAttribute("vgrVardval"));
+
+        return unit;
+    }
+}
